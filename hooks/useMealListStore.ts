78400c830f163b95c92baf3e8c36b413a3e3bfc1
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2024-07-07 14:02:19 +0800

    Amend dummy data

--- hooks/useMealListStore.ts
+++ hooks/useMealListStore.ts
@@ -1,21 +1,84 @@
 import { create } from "zustand";
-
 // Dummy data
-const dummyMeals = [
-  "Ikan masak merah",
-  "Nasi Lemak",
-  "Ayam masak lemak cili api",
-  "Kentang goreng",
-  "Ayam panggang",
-  "Salmon mentai",
+const dummyMeals: MealSelection[] = [
+  {
+    id: "meal-1",
+    meal_name: "Ikan masak merah",
+    description: "A rich and flavorful fish dish cooked in a red chili sauce.",
+    item_list: [
+      // { item_name: "Fish Fillet", quantity: 1 },
+      // { item_name: "Red Chili Sauce", quantity: 2 }
+    ],
+  },
+  {
+    id: "meal-2",
+    meal_name: "Nasi Lemak",
+    description:
+      "Traditional Malaysian dish with rice cooked in coconut milk, served with various accompaniments.",
+    item_list: [
+      // { item_name: "Coconut Rice", quantity: 1 },
+      // { item_name: "Sambal", quantity: 1 },
+      // { item_name: "Fried Anchovies", quantity: 1 },
+      // { item_name: "Boiled Egg", quantity: 1 }
+    ],
+  },
+  {
+    id: "meal-3",
+    meal_name: "Ayam masak lemak cili api",
+    description:
+      "A spicy and creamy chicken dish cooked with turmeric and coconut milk.",
+    item_list: [
+      // { item_name: "Chicken", quantity: 1 },
+      // { item_name: "Turmeric", quantity: 1 },
+      // { item_name: "Coconut Milk", quantity: 2 }
+    ],
+  },
+  {
+    id: "meal-4",
+    meal_name: "Kentang goreng",
+    description:
+      "Crispy and golden French fries, a perfect snack or side dish.",
+    item_list: [
+      // { item_name: "Potatoes", quantity: 2 },
+      // { item_name: "Salt", quantity: 1 },
+      // { item_name: "Oil", quantity: 1 }
+    ],
+  },
+  {
+    id: "meal-5",
+    meal_name: "Ayam panggang",
+    description: "Juicy and flavorful grilled chicken, seasoned to perfection.",
+    item_list: [
+      // { item_name: "Chicken", quantity: 1 },
+      // { item_name: "Grill Seasoning", quantity: 1 }
+    ],
+  },
+  {
+    id: "meal-6",
+    meal_name: "Salmon mentai",
+    description:
+      "Delicious salmon topped with creamy mentai sauce, broiled to perfection.",
+    item_list: [
+      // { item_name: "Salmon Fillet", quantity: 1 },
+      // { item_name: "Mentai Sauce", quantity: 1 }
+    ],
+  },
 ];
 
+const dummyAddNewMeal = {
+  id: "meal-7",
+  meal_name: "Chicken wrap",
+  description:
+    "Delicious salmon topped with creamy mentai sauce, broiled to perfection.",
+  item_list: [
+    // { item_name: "Salmon Fillet", quantity: 1 },
+    // { item_name: "Mentai Sauce", quantity: 1 }
+  ],
+};
+
 const useMealListStore: any = create((set: any) => ({
-  mealList: dummyMeals,
+  mealList: dummyMeals as MealSelection[],
   addNewMeal: () => addNewMeal(set),
-  increasePopulation: () => set((state: any) => ({ bears: state.bears + 1 })),
-  removeAllBears: () => set({ bears: 0 }),
-  updateBears: (newBears: any) => set({ bears: newBears }),
 }));
 
 export default useMealListStore;
@@ -24,7 +87,7 @@ const addNewMeal = (set: any) => {
   console.log("New meal is added :)");
   set(({ mealList }: any) => {
     return {
-      mealList: [...mealList, "New item 1"],
+      mealList: [...mealList, dummyAddNewMeal],
     };
   });
 };
