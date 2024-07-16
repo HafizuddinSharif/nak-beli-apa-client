68970b81837c7afba98ceb6ef565f1ff064cb65e
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2024-07-16 19:35:08 +0800

    Add placeholder if no content

--- components/view-meal-page/MealDetails.tsx
+++ components/view-meal-page/MealDetails.tsx
@@ -1,6 +1,16 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 
 const MealDetails = ({ details }: any) => {
+  if (!details) {
+    return (
+      <View
+        style={{ height: "100%", justifyContent: "center", paddingBottom: 200 }}
+      >
+        <Text style={{ fontSize: 16, textAlign: "center" }}>To be build</Text>
+      </View>
+    );
+  }
+
   return <Text style={{ fontSize: 16 }}>{details}</Text>;
 };
 
--- components/view-meal-page/MealSteps.tsx
+++ components/view-meal-page/MealSteps.tsx
@@ -1,8 +1,19 @@
-import { Text } from "react-native";
+import { COLORS } from "@/constants";
+import { Text, View } from "react-native";
 
 const MealSteps = ({ cooking_guide }: any) => {
-  if (cooking_guide == "") {
-    return <Text style={{ fontSize: 16 }}>Nampaknya its empty</Text>;
+  if (!cooking_guide) {
+    return (
+      <View
+        style={{ height: "100%", justifyContent: "center", paddingBottom: 200 }}
+      >
+        <Text
+          style={{ fontSize: 16, textAlign: "center", color: COLORS.gray2 }}
+        >
+          Bahagian ini belum siap dibina
+        </Text>
+      </View>
+    );
   }
   return <Text style={{ fontSize: 16 }}>{cooking_guide}</Text>;
 };
