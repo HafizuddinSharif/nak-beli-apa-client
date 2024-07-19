802296c68298e416a12585415f1b61d10993309b
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2024-07-19 16:18:38 +0800

    Add fetchMealList function

--- hooks/useMealListStore.ts
+++ hooks/useMealListStore.ts
@@ -2,13 +2,19 @@ import { create } from "zustand";
 import { dummyMeals, dummyAddNewMeal } from "@/dummy_data";
 
 const useMealListStore: any = create((set: any) => ({
-  mealList: dummyMeals as MealSelection[],
+  mealList: [] as MealSelection[],
   addNewMeal: (newMeal: MealSelection) => addNewMeal(set, newMeal),
   removeMeal: (mealId: string) => removeMeal(set, mealId),
+  fetchMealList: (payload) => fetchMealList(set, payload),
 }));
 
 export default useMealListStore;
 
+const fetchMealList = (set: any, payload: any) => {
+  set(() => {
+    return { mealList: payload };
+  });
+};
 const addNewMeal = (set: any, newMeal: MealSelection) => {
   set(({ mealList }: any) => {
     return {
