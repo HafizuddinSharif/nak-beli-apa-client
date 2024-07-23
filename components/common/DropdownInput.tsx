8dc0bf1a39c77bba302fedd0b32f94c1503c9401
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2024-07-23 22:04:32 +0800

    Update dummyOption to have less options

--- components/common/DropdownInput.tsx
+++ components/common/DropdownInput.tsx
@@ -16,14 +16,7 @@ const DropdownInput = ({
   const dummyOption = [
     { label: "Item 1", value: "1" },
     { label: "Item 2", value: "2" },
-    { label: "Item 3", value: "3" },
-    { label: "Item 4", value: "4" },
-    { label: "Item 5", value: "5" },
-    { label: "Item 6", value: "6" },
-    { label: "Item 7", value: "7" },
-    { label: "Item 8", value: "8" },
   ];
-
   const renderLabel = () => {
     if (value || isFocus) {
       return (
