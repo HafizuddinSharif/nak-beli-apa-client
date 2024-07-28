95e8a759c93220bc487a3acb4331e201a2f9be79
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2024-07-28 22:26:40 +0800

    UPDATE: After save meal, go to the meal page

--- app/edit-meal/[id].tsx
+++ app/edit-meal/[id].tsx
@@ -97,7 +97,7 @@ export default function EditMealPage() {
       DB.updateMeal(db, updatedMeal);
     }
 
-    router.push(`/meals`);
+    router.navigate(`/view-meal/${meal.id ? meal.id : mealList.length + 1}`);
   };
 
   return (
