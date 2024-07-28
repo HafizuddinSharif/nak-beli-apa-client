9451f81a778ae89db4d6029dae6dc13850c82950
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2024-07-28 22:38:59 +0800

    ADD: New text for edit meal page

--- constants/text.ts
+++ constants/text.ts
@@ -4,6 +4,10 @@ const TEXT = {
       BM: "Tambah masakan baru",
       EN: "Add a new meal",
     },
+    TITLE_EDIT: {
+      BM: "Ubah masakan",
+      EN: "Edit meal",
+    },
     MENU_NAME_PLACEHOLDER: {
       BM: "Nama masakan",
       EN: "Meal name",
